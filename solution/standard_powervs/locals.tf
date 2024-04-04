fd66be5c781b03b59d60640554f65556ac39855b
maulikcapg <maulik.patadia@capgemini.com>
2024-04-04 01:23:13 -0500

    feat: switch syd05 DC to PER

--- solution/standard_powervs/locals.tf
+++ solution/standard_powervs/locals.tf
@@ -61,7 +61,7 @@ locals {
   # IBM Cloud PowerVS Configuration
   #####################################################
 
-  pi_per_enabled_dc_list = ["dal10", "dal12", "wdc06", "wdc07", "mad02", "mad04", "eu-de-1", "eu-de-2", "sao01", "sao04", "tok04", "osa21"]
+  pi_per_enabled_dc_list = ["dal10", "dal12", "wdc06", "wdc07", "mad02", "mad04", "eu-de-1", "eu-de-2", "sao01", "sao04", "sao05", "tok04", "osa21"]
   pi_per_enabled         = contains(local.pi_per_enabled_dc_list, var.powervs_zone)
   cloud_connection_count = local.pi_per_enabled ? 0 : var.cloud_connection.count
 
