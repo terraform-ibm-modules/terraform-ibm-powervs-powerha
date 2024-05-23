4c91881927149a147a90dd05808e811946fae2d3
IBMBDGhataliya <Baldev.Ghataliya@partner.ibm.com>
2024-05-23 04:30:25 -0500

    fix: ansible tar update

--- solutions/glvm_powervs/variables.tf
+++ solutions/glvm_powervs/variables.tf
@@ -4,11 +4,11 @@ variable "prerequisite_workspace_id" {
 }
 
 variable "prefix" {
-  description = "A unique identifier for resources. The identifier must begin with a lowercase letter and end with a lowercase letter or a number. This prefix will be prepended to any resources provisioned by this template. Prefix must be 8 characters or fewer than 8 characters."
+  description = "A unique identifier for resources. The identifier must begin with a lowercase letter and end with a lowercase letter or a number. This prefix will be prepended to any resources provisioned by this template. Prefix should between 1 to 8 characters."
   type        = string
   validation {
     condition     = can(regex("^[a-zA-Z][a-zA-Z0-9-_]{0,7}$", var.prefix))
-    error_message = "The prefix must begin with an alphabetic character followed by an alphanumeric character, an underscore, and a hyphen. Prefixes must be a maximum of 8  characters."
+    error_message = "The prefix must begin with an alphabetic character followed by an alphanumeric character, an underscore, and a hyphen. Prefix should between 1 to 8 characters."
   }
 }
 
@@ -161,6 +161,7 @@ variable "shared_volume" {
 variable "cos_powerha_image_download" {
   description = <<EOT
   Details about cloud object storage bucket where PowerHA installation media folder and ssl file are located. For more details click [here](https://cloud.ibm.com/docs/cloud-object-storage?topic=cloud-object-storage-service-credentials).
+  ssl_file_name is only applicable to AIX 7.2 and can be skipped for AIX 7.3.
   Example:
     {
       "bucket_name":"bucket-name",
@@ -185,7 +186,6 @@ variable "cos_powerha_image_download" {
     ssl_file_name         = string
   })
 }
-
 
 variable "powerha_glvm_volume_group" {
   description = "Number of Volume Groups which need to be created in PowerHA."
@@ -216,7 +216,7 @@ variable "site2_cloud_connection" {
 }
 
 variable "site1_custom_profile" {
-  description = "Overrides t-shirt profile for site1: Custom PowerVS instance. Specify combination of cores, memory, proc_type and storage tier."
+  description = "Overrides t-shirt profile for site1: Custom PowerVS instance. Specify combination of cores, memory, proc_type and storage tier. For more details click [here](https://cloud.ibm.com/docs/power-iaas?topic=power-iaas-creating-power-virtual-server#creating-power-virtual-server%22)."
   type = object({
     cores     = number
     memory    = number
@@ -236,7 +236,7 @@ variable "site1_custom_profile" {
 }
 
 variable "site2_custom_profile" {
-  description = "Overrides t-shirt profile for site2: Custom PowerVS instance. Specify combination of cores, memory, proc_type and storage tier."
+  description = "Overrides t-shirt profile for site2: Custom PowerVS instance. Specify combination of cores, memory, proc_type and storage tier. For more details click [here](https://cloud.ibm.com/docs/power-iaas?topic=power-iaas-creating-power-virtual-server#creating-power-virtual-server%22)."
   type = object({
     cores     = number
     memory    = number
@@ -278,7 +278,6 @@ variable "shared_volume_attributes" {
     error_message = "Shared Volume Size should be between 10 and 10000 and tier should be tier0, tier1, tier3, fixed IOPS."
   }
 }
-
 
 variable "powerha_glvm_volume_group_list" {
   description = "List of parameters for volume group - Individual PowerHA volume group configuration. Based on the volume_group count, you can provide all the volume group configuration like name, resource group name, type, size, tier. Default configuration will be taken if details are not provided."
